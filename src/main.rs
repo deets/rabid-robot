0311364ba6450db1a5c0f22050f71b6d36f5f807
Diez B. Roggisch <deets@web.de>
2020-04-25 14:21:52 +0200

    Communicate current state (only voltage so far) back
    Also the reading of the voltage keeps the watchdog in the MD23 happy

--- src/main.rs
+++ src/main.rs
@@ -12,22 +12,33 @@ enum Message
     Drive{speed: f32},
 }
 
-enum State
+enum Mode
 {
     Normal,
     LowVoltage
 }
 
+enum State {
+    Battery{voltage: f32}
+}
+
+use State::Battery;
+
 struct MD23Driver {
     outgoing: std::sync::mpsc::Sender<Message>,
+    incoming: std::sync::mpsc::Receiver<State>,
 }
 
 impl MD23Driver {
 
-    fn start_thread(rx: std::sync::mpsc::Receiver<Message>, addr: u16)
+    fn start_thread(
+        rx: std::sync::mpsc::Receiver<Message>,
+        tx: std::sync::mpsc::Sender<State>,
+        addr: u16
+    )
     {
         thread::spawn(move || {
-            let mut state = State::Normal;
+            let mut mode = Mode::Normal;
             let mut dev = match LinuxI2CDevice::new("/dev/i2c-1", addr)
             {
                 Ok(dev) => dev,
@@ -40,12 +51,17 @@ impl MD23Driver {
                     match message {
                         Message::Drive{speed} => {
                             let speed = (speed * 127.0 + 128.0) as u8;
-                            println!("speed: {}", speed);
                             dev.smbus_write_byte_data(0, speed);
                             dev.smbus_write_byte_data(1, speed);
                         }
                     }
                 }
+                let voltage = match dev.smbus_read_byte_data(10) {
+                    Ok(voltage) => voltage as f32,
+                    Err(err) => panic!("error reading battery: {}", err)
+                } / 10.0;
+                tx.send(Battery{voltage: voltage});
+                thread::sleep(Duration::from_millis(100));
             }
         });
     }
@@ -53,33 +69,54 @@ impl MD23Driver {
     pub fn new(addr: u16) -> MD23Driver
     {
         let (tx, rx) = mpsc::channel();
-        MD23Driver::start_thread(rx, addr);
+        let (tx_incoming, rx_incoming) = mpsc::channel();
+        MD23Driver::start_thread(rx, tx_incoming, addr);
         MD23Driver{
-            outgoing: tx
+            outgoing: tx,
+            incoming: rx_incoming
         }
     }
 
-    pub fn drive(self: &mut MD23Driver, speed: f32)
+    fn gather_state_messages(self: &mut MD23Driver) -> Vec<State>
+    {
+        let mut result = Vec::new();
+        result.extend(self.incoming.try_iter());
+        return result;
+    }
+
+    pub fn drive(self: &mut MD23Driver, speed: f32) -> Vec<State>
     {
         self.outgoing.send(Message::Drive{speed: speed});
+        self.gather_state_messages()
     }
 
-    pub fn stop(self: &mut MD23Driver)
+    pub fn stop(self: &mut MD23Driver) -> Vec<State>
     {
         self.outgoing.send(Message::Drive{speed: 0.0});
+        self.gather_state_messages()
     }
+}
 
+fn output_state(states: &Vec<State>)
+{
+    for state in states.iter() {
+        match state {
+            Battery{voltage} => println!("voltage: {}", voltage)
+        }
+    }
 }
 fn main() {
     let mut md23 = MD23Driver::new(MD23_ADDR);
     loop {
-        thread::sleep(Duration::from_millis(1000));
-        md23.drive(0.5);
-        thread::sleep(Duration::from_millis(1000));
-        md23.stop();
-        thread::sleep(Duration::from_millis(1000));
+        thread::sleep(Duration::from_millis(4000));
+        let res = md23.drive(0.5);
+        output_state(&res);
+        thread::sleep(Duration::from_millis(4000));
+        let res = md23.stop();
+        output_state(&res);
+        thread::sleep(Duration::from_millis(4000));
         md23.drive(-0.5);
-        thread::sleep(Duration::from_millis(1000));
+        thread::sleep(Duration::from_millis(4000));
         md23.stop();
     }
 }
