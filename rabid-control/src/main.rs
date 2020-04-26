afaddb074b63ca2d2fc3e070f0ecc81f96d96f5d
Diez B. Roggisch <deets@web.de>
2020-04-26 10:49:02 +0200

    Restructure repo

--- rabid-control/src/main.rs
+++ rabid-control/src/main.rs
@@ -108,15 +108,15 @@ fn output_state(states: &Vec<State>)
 fn main() {
     let mut md23 = MD23Driver::new(MD23_ADDR);
     loop {
-        thread::sleep(Duration::from_millis(4000));
+        thread::sleep(Duration::from_millis(2000));
         let res = md23.drive(0.5);
         output_state(&res);
-        thread::sleep(Duration::from_millis(4000));
+        thread::sleep(Duration::from_millis(2000));
         let res = md23.stop();
         output_state(&res);
-        thread::sleep(Duration::from_millis(4000));
+        thread::sleep(Duration::from_millis(2000));
         md23.drive(-0.5);
-        thread::sleep(Duration::from_millis(4000));
+        thread::sleep(Duration::from_millis(2000));
         md23.stop();
     }
 }
